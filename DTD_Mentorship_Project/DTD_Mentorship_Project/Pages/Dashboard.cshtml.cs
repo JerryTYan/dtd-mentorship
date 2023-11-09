8b473c90b4595f036fca15761e3b50c2142ddb75
MSainz3 <145814570+MSainz3@users.noreply.github.com>
2023-11-08 16:47:47 -0800

    Dashboard cs
    made cs file for dashboard.

---
+++ DTD_Mentorship_Project/DTD_Mentorship_Project/Pages/Dashboard.cshtml.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DTD_Mentorship_Project.Pages
+{
+    public class DashboardModel : PageModel
+    {
+        private readonly ILogger<DashboardModel> _logger;
+
+        // Define properties here
+        public string Name { get; set; }
+        public string District { get; set; }
+        public List<Mentee> CurrentMentees { get; set; }
+        public string AboutMe { get; set; }
+
+        public DashboardModel(ILogger<DashboardModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            // Initialize properties here
+            Name = "John Doe";
+            District = "District 1";
+            CurrentMentees = new List<Mentee>
+       {
+           new Mentee { Name = "Mentee 1" },
+           new Mentee { Name = "Mentee 2" },
+           // Add more mentees here
+       };
+            AboutMe = "About me...";
+        }
+    }
