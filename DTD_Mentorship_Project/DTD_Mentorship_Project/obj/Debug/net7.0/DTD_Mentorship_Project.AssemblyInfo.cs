3db07402060a36afd164730fde2631973381474a
1423BMTH <mmyyproj@gmail.com>
2023-11-18 14:32:04 +0800

    Submission page
    Fixing css

--- DTD_Mentorship_Project/DTD_Mentorship_Project/obj/Debug/net7.0/DTD_Mentorship_Project.AssemblyInfo.cs
+++ DTD_Mentorship_Project/DTD_Mentorship_Project/obj/Debug/net7.0/DTD_Mentorship_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DTD_Mentorship_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c169fbd390ead14f48cffc478d901b0f6a9a1496")]
 [assembly: System.Reflection.AssemblyProductAttribute("DTD_Mentorship_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DTD_Mentorship_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
