b30c1771c2ad4ef490aa49e71d39292714b531e5
1423BMTH <mmyyproj@gmail.com>
2023-11-20 13:25:04 -0800

    height of submission

--- DTD_Mentorship_Project/DTD_Mentorship_Project/obj/Debug/net7.0/DTD_Mentorship_Project.AssemblyInfo.cs
+++ DTD_Mentorship_Project/DTD_Mentorship_Project/obj/Debug/net7.0/DTD_Mentorship_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DTD_Mentorship_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35366f5e38534f87f25dbd3ffdf2ae7b7254617e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+107bc48424bdd1c8afc8160471243b03d5b76635")]
 [assembly: System.Reflection.AssemblyProductAttribute("DTD_Mentorship_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DTD_Mentorship_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
