775992fc6c1e955ef5c9500500485fe463486e70
5014NG3 <eddygalvez@rocketmail.com>
2023-11-30 13:39:07 -0800

    eg - added backend for the mentorship page
    we need to integrate db to make this work correctly. This would require initially signing in with email, getting user data and then using this data across all pages to display data. in the case of the mentorship page, tabs are displayed based on if the user is mentor or mentee

--- DTD_Mentorship_Project/DTD_Mentorship_Project/Pages/Mentorship.cshtml.cs
+++ DTD_Mentorship_Project/DTD_Mentorship_Project/Pages/Mentorship.cshtml.cs
@@ -3,10 +3,25 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DTD_Mentorship_Project.Pages
 {
+    public class UserModel
+    {
+        public string? UserId { get; set; }
+        public string? UserType { get; set; }
+    }
+
     public class MentorshipModel : PageModel
     {
+
+        public UserModel? CurrentUser { get; set; }
+
+
+
         public void OnGet()
         {
+            //switch between mentee and mentor to see different stuff on page
+            //ideally we use userid to get userdat from db, and then determine if page
+            //is meant for mentor or mentee
+            CurrentUser = new UserModel {UserId = "45", UserType = "mentor"};
         }
     }
 }
