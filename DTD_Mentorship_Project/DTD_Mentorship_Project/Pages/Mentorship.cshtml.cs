5a61d46fb6ffa809ea2e4445c4a36dcd0ee302a5
5014NG3 <eddygalvez@rocketmail.com>
2023-12-01 10:11:02 -0800

    eg - added backend for account merge and beautified all pages
    I created a new model named user which is just a placeholder for when we use login details to retrieve userdata. I modified the mentor select page to use the mentor model instead of the inline model I created, did the same for the mentorship page which now uses the "user" model instead of the inline model I had. lastly, I used visual studio document formatting to make the html structure easier to read in all of the pages.

--- DTD_Mentorship_Project/DTD_Mentorship_Project/Pages/Mentorship.cshtml.cs
+++ DTD_Mentorship_Project/DTD_Mentorship_Project/Pages/Mentorship.cshtml.cs
@@ -1,18 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using DTD_Mentorship_Project.Models;
+
+
 
 namespace DTD_Mentorship_Project.Pages
 {
-    public class UserModel
-    {
-        public string? UserId { get; set; }
-        public string? UserType { get; set; }
-    }
+
 
     public class MentorshipModel : PageModel
     {
 
-        public UserModel? CurrentUser { get; set; }
+        public User? CurrentUser { get; set; }
 
 
 
@@ -21,7 +20,7 @@ namespace DTD_Mentorship_Project.Pages
             //switch between mentee and mentor to see different stuff on page
             //ideally we use userid to get userdat from db, and then determine if page
             //is meant for mentor or mentee
-            CurrentUser = new UserModel {UserId = "45", UserType = "mentor"};
+            CurrentUser = new User {UserId = "45", UserType = "mentee"};
         }
     }
 }
