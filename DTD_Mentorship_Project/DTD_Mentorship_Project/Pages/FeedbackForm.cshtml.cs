9809889eefcd733e225129ab4b4e49201cab55a0
5014NG3 <eddygalvez@rocketmail.com>
2023-12-04 14:03:52 -0800

    eg - implement backend for feedback pages

--- DTD_Mentorship_Project/DTD_Mentorship_Project/Pages/FeedbackForm.cshtml.cs
+++ DTD_Mentorship_Project/DTD_Mentorship_Project/Pages/FeedbackForm.cshtml.cs
@@ -5,8 +5,20 @@ namespace DTD_Mentorship_Project.Pages
 {
     public class FeedbackFormModel : PageModel
     {
+
+        public string? MentorImage { get; set; }
+        public string? MentorName { get; set; }
+        public string? MentorDescription { get; set; }
+        public string? MentorTitle { get; set; }
+        public string? MentorArea { get; set; }
+
         public void OnGet()
         {
+            MentorImage = Request.Query["image"];
+            MentorName = Request.Query["name"];
+            MentorDescription = Request.Query["description"];
+            MentorTitle = Request.Query["title"];
+            MentorArea = Request.Query["area"];
         }
     }
 }
