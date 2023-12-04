0c70bd0fb8c8cb75880657aaa5c61c262fc311dd
5014NG3 <eddygalvez@rocketmail.com>
2023-12-04 10:16:29 -0800

    Revert "Merge branch 'main' into feature/mentor-select-page"
    This reverts commit efce8926c72a59467eb0d15ebeefcc7e46c059dd, reversing
    changes made to ad05db5b9faf535a5c8b767b28ef7b1ee308ff1f.

--- DTD_Mentorship_Project/DTD_Mentorship_Project/Models/Mentor.cs
+++ DTD_Mentorship_Project/DTD_Mentorship_Project/Models/Mentor.cs
@@ -2,10 +2,5 @@
 {
     public class Mentor
     {
-        public string? Name { get; set; }
-        public string? Description { get; set; }
-        public string? Title { get; set; }
-        public string? Area { get; set; }
-        public string? Image { get; set; }
     }
 }
--- DTD_Mentorship_Project/DTD_Mentorship_Project/Models/User.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DTD_Mentorship_Project.Models
-{
-    public class User
-    {
-        public string? UserId { get; set; }
-        public string? UserType { get; set; }
-    }
-}
