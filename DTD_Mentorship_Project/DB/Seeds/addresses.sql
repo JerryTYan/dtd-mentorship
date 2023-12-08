777719832f7c156a4182ebfc8e00d01c1ae88d25
Ivan Flores <iflores@isd.lacounty.gov>
2023-12-08 12:40:54 -0800

    importing all tables into solution

---
+++ DTD_Mentorship_Project/DB/Seeds/addresses.sql
@@ -0,0 +1,6 @@
+INSERT INTO [$(Address)]([UserID],[Street_Address],[City],[State],[Zip_Code]) VALUES (1, '123 Main St', 'Los Angeles', 'CA', '90001');
+INSERT INTO [$(Address)]([UserID],[Street_Address],[City],[State],[Zip_Code]) VALUES (2, '456 Oak Ave', 'Long Beach', 'CA', '90802');
+INSERT INTO [$(Address)]([UserID],[Street_Address],[City],[State],[Zip_Code]) VALUES (3, '789 Pine Blvd', 'Pasadena', 'CA', '91101');
+INSERT INTO [$(Address)]([UserID],[Street_Address],[City],[State],[Zip_Code]) VALUES (4, '101 Broadway St', 'Santa Monica', 'CA', '90401');
+INSERT INTO [$(Address)]([UserID],[Street_Address],[City],[State],[Zip_Code]) VALUES (5, '202 Sunset Rd', 'Glendale', 'CA', '91201');
+INSERT INTO [$(Address)]([UserID],[Street_Address],[City],[State],[Zip_Code]) VALUES (6, '303 Elm Ln', 'Burbank', 'CA', '91501');
