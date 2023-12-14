435d8dd03b6c3bfb42c9191f8f75db6dd20c2492
5014NG3 <eddygalvez@rocketmail.com>
2023-12-14 13:37:36 -0800

    eg - fixed rest user seeds, changed ID to INT

--- DTD_Mentorship_Project/DB/Tables/User.sql
+++ DTD_Mentorship_Project/DB/Tables/User.sql
@@ -7,7 +7,7 @@
 	[Password] NVARCHAR(200),
 	[OngoingMentorship] BIT NOT NULL,
 	[Image] NVARCHAR(200), 
-    [Identity] NVARCHAR(200) NOT NULL, 
+    [Identity] INT NOT NULL, 
     [Degree] NVARCHAR(200) NULL, 
     [Availability] DATETIME NULL
 )
