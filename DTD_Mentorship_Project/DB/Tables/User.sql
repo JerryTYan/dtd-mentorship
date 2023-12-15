1dcfc7eccdf239c5b0f9c28441fa52066034b063
5014NG3 <eddygalvez@rocketmail.com>
2023-12-15 12:44:30 -0800

    eg - login page query db

--- DTD_Mentorship_Project/DB/Tables/User.sql
+++ DTD_Mentorship_Project/DB/Tables/User.sql
@@ -7,7 +7,7 @@
 	[Password] NVARCHAR(200),
 	[OngoingMentorship] BIT NOT NULL,
 	[Image] NVARCHAR(200), 
-    [Identity] INT NOT NULL, 
+    [IdentityId] INT NOT NULL, 
     [Degree] NVARCHAR(200) NULL, 
     [Availability] DATETIME NULL
 )
--- DTD_Mentorship_Project/DTD_Mentorship_Project/Pages/Account/Login.cshtml.cs
+++ DTD_Mentorship_Project/DTD_Mentorship_Project/Pages/Account/Login.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
+using DTD_Mentorship_Project.Models;
 using System.Globalization;
 
 namespace DTD_Mentorship_Project.Pages
@@ -9,13 +10,17 @@ namespace DTD_Mentorship_Project.Pages
     public class LoginModel : PageModel
     {
         private readonly ILogger _logger;
+        private readonly DBContext _dbContext;
+
 
         [BindProperty]
         public LoginFormData FormData { get; set; }
 
-        public LoginModel(ILogger<LoginModel> logger)
+        public LoginModel(Models.DBContext db, ILogger<LoginModel> logger)
         {
             _logger = logger;
+            _dbContext = db;
+
         }
 
         public void OnGet()
@@ -28,6 +33,27 @@ namespace DTD_Mentorship_Project.Pages
             {
                 var email = FormData.Email;
                 var password = FormData.Password;
+
+                var user = _dbContext.Users.SingleOrDefault(user => user.Email == email && user.Password == password);
+                var identity = _dbContext.Identities.SingleOrDefault(identity => identity.Id == user.IdentityId);
+
+                var identity_name = identity.IdentityName;   
+
+                //var user = _dbContext.Users.ToList();
+
+
+
+                if (user != null)
+                {
+                    _logger.LogInformation("User found: {User}", user.LastName);
+                    _logger.LogInformation("Id found: {IdName}", identity_name);
+
+
+                }
+                else
+                {
+                    _logger.LogInformation("No user found for email: {Email}", email);
+                }
 
                 // TODO: Add authentication logic here
 
